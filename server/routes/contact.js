05b381370a1f6df38375d2facca29a93510c9c48
Vinnu181630 <vinay.gudarapu@outlook.com>
2026-04-30 20:27:49 +0530

    corrected the pathname contact

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Contact = require("../models/Contact");
+const Contact = require("../models/contact");
 
 // POST API
 router.post("/", async (req, res) => {
